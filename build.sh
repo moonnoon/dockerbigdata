e0326040c371f9cdfc272531bc1882abc41e22d2
Fei Lee <lee.lfff@gmail.com>
2014-08-06 15:29:43 +0800

    add flume config

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ sudo docker build -t moonnoon/base ./base/
 sudo docker build -t moonnoon/zookeeper ./zookeeper/
 sudo docker build -t moonnoon/kafka ./kafka/
 sudo docker build -t moonnoon/flume ./flume/
+sudo docker build -t moonnoon/hadoop ./hadoop
