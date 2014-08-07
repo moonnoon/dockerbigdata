f23f9258550e0dd6e2380cfafcec5b4ab38a0870
Fei Lee <lee.lfff@gmail.com>
2014-08-07 16:57:14 +0800

    add elasticsearch, unfinished...

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ sudo docker build -t moonnoon/zookeeper ./zookeeper/
 sudo docker build -t moonnoon/kafka ./kafka/
 sudo docker build -t moonnoon/flume ./flume/
 sudo docker build -t moonnoon/hadoop ./hadoop
+sudo docker build -t moonnoon/elasticsearch ./elasticsearch/
