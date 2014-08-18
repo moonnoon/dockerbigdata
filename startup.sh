d6d0e3b21b9f73dea76786c8df3bda69d2d1f46a
Fei Lee <lee.lfff@gmail.com>
2014-08-18 16:59:17 +0800

    replace sleep with logs

--- startup.sh
+++ startup.sh
@@ -15,14 +15,41 @@ sudo docker rm -f storm 1>&- 2>&-
 #run
 sudo docker run -d -P --name data moonnoon/data:testing
 sudo docker run -d -P --volumes-from data --name zookeeper moonnoon/zookeeper:testing
-sleep 1s
+#waiting for zookeeper finish start
+attempt=0
+while [ $attempt -le 59 ]; do
+	attempt=$(( $attempt + 1 ))
+	result=$(sudo docker logs zookeeper 2>&1)
+	if grep -q 'binding to port 0.0.0.0/0.0.0.0:2181' <<< $result ; then
+		break
+	fi
+	sleep 1
+done
 sudo docker run -d -P --volumes-from data --name kafka --link zookeeper:zookeeper moonnoon/kafka:testing
-sleep 1s
+attempt=0
+while [ $attempt -le 59 ]; do
+	attempt=$(( $attempt + 1 ))
+	result=$(sudo docker logs kafka 2>&1)
+	if grep -q 'INFO success: kafka entered RUNNING state' <<< $result ; then
+		break
+	fi
+	sleep 1
+done
 sudo docker run -d -P --volumes-from data --name storm --link zookeeper:zookeeper moonnoon/storm:testing
 ID=$(sudo docker run -d -P --volumes-from data --name appflume --link zookeeper:zookeeper --link kafka:kafka moonnoon/appflume:testing)
 sudo docker run -d -P --volumes-from data --name hadoop --link zookeeper:zookeeper  moonnoon/hadoop:testing
 sudo docker run -d -P --volumes-from data --name elasticsearch --link zookeeper:zookeeper moonnoon/elasticsearch:testing
-sleep 5s
+
+#waiting for hadoop finish start
+while [ $attempt -le 59 ]; do
+	attempt=$(( $attempt + 1 ))
+	result=$(sudo docker logs hadoop 2>&1)
+	if grep -q 'INFO exited: dfs (exit status 0; expected)' <<< $result ; then
+		break
+	fi
+	sleep 1
+done
+
 sudo docker run -d -P --volumes-from data --name flume --link zookeeper:zookeeper --link hadoop:hadoop --link elasticsearch:elasticsearch moonnoon/flume:testing
 
 #
