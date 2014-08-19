a4ad25f76f3b37b3123577dff6495920ce41c1d5
Fei Lee <lee.lfff@gmail.com>
2014-08-19 10:18:29 +0800

    have fun

--- storm/startup.sh
+++ storm/startup.sh
@@ -2,9 +2,6 @@
 #
 #startup.sh
 
-sed -i "s/zookeeper_ip/$(cat /etc/hosts | grep zookeeper | cut -f1)/" /opt/storm/examples/storm-starter/src/jvm/storm/starter/Test.java
-
-mvn package
 
 #run
 /opt/storm/bin/storm jar target/storm-starter-0.9.2-incubating-jar-with-dependencies.jar storm.starter.Test
