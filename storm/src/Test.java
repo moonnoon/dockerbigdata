a6ce789362173660a63353c5935177a5a0784bfd
Fei Lee <lee.lfff@gmail.com>
2014-08-20 16:53:10 +0800

    add ip analysis

--- storm/src/Test.java
+++ storm/src/Test.java
@@ -35,6 +35,12 @@ import storm.kafka.*;
 
 
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import java.net.InetAddress;
 
 /**
@@ -42,53 +48,101 @@ import java.net.InetAddress;
  */
 public class Test {
 
-        public static class MyBolt extends BaseRichBolt {
-                OutputCollector _collector;
+	public static class MyBolt extends BaseRichBolt {
+		OutputCollector _collector;
+		static TopIP ip = new TopIP();
 
-                @Override
-                public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
-                        _collector = collector;
-                }
+		@Override
+		public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+			_collector = collector;
+		}
 
-                @Override
-                public void execute(Tuple tuple) {
-                        _collector.emit(tuple, new Values(
-									"================ " + tuple.getValues() + " ============="));
-                        _collector.ack(tuple);
-                }
+		@Override
+		public void execute(Tuple tuple) {
+			//_collector.emit(tuple, new Values(
+			//			"================ " + tuple.getValues() + " ============="));
+			//_collector.ack(tuple);
+			ip.putIP(ip.getIPFromString(tuple.getString(0)));
+		}
 
-                @Override
-                public void declareOutputFields(OutputFieldsDeclarer declarer) {
-                        declarer.declare(new Fields("message"));
-                }
+		@Override
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {
+			declarer.declare(new Fields("message"));
+		}
 
 
-        }
+	}
 
-        public static void main(String[] args) throws Exception {
-                TopologyBuilder builder = new TopologyBuilder();
+	public static class TopIP {
+		private Map<String, Integer> map;
 
-                BrokerHosts hosts = new ZkHosts(InetAddress.getByName("zookeeper").getHostAddress() 
-						+ ":2181");
-                SpoutConfig spoutConfig = new SpoutConfig(
-                                hosts,
-                                "test",
-                                "/kafkastorm",
-                                "discovery");
-                //spoutConfig.scheme = new RawMultiScheme();
-                spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+		public TopIP() {
+			map = new HashMap<String, Integer>();
+		}
+		public void putIP(String ip) {
+			if (map.containsKey(ip)) {
+				map.put(ip, map.get(ip) + 1);
+			} else {
+				map.put(ip, 1);
+			}
+		}
+		public String getIPFromString(String str) {
+			String regex = "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}";
+			Pattern p = Pattern.compile(regex);
+			Matcher m = p.matcher(str);
+			if(m.find()) {
+				return m.group();
+			}
+			return null;
+		}
+		public Map<String, Integer> getTopIP() {
+			TreeMap<String, Integer> tmap = new TreeMap<String, Integer>(new ValueComparator(map));
+			tmap.putAll(map);
+			return tmap;
+		}
 
-                builder.setSpout("kafka", new KafkaSpout(spoutConfig));
-                builder.setBolt("bolt", new MyBolt()).shuffleGrouping("kafka");
+		class ValueComparator implements Comparator<String> {
+			Map<String, Integer> base;
+			public ValueComparator(Map<String, Integer> base) {
+				this.base = base;
+			}
+			public int compare(String a, String b) {
+				if (base.get(a) >= base.get(b)) {
+					return -1;
+				} else {
+					return 1;
+				}
+			}
+		}
+	}
 
-                Config conf = new Config();
-                conf.setDebug(true);
+	public static void main(String[] args) throws Exception {
+		TopologyBuilder builder = new TopologyBuilder();
 
-                LocalCluster cluster = new LocalCluster();
-                cluster.submitTopology("test", conf, builder.createTopology());
+		BrokerHosts hosts = new ZkHosts(InetAddress.getByName("zookeeper").getHostAddress() 
+				+ ":2181");
+		SpoutConfig spoutConfig = new SpoutConfig(
+				hosts,
+				"test",
+				"/kafkastorm",
+				"discovery");
+		//spoutConfig.scheme = new RawMultiScheme();
+		spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
 
-                //Utils.sleep(200000);
-                //cluster.killTopology("test");
-                //cluster.shutdown();
-        }
+		builder.setSpout("kafka", new KafkaSpout(spoutConfig));
+		builder.setBolt("bolt", new MyBolt()).shuffleGrouping("kafka");
+
+		Config conf = new Config();
+		conf.setDebug(true);
+
+		LocalCluster cluster = new LocalCluster();
+		cluster.submitTopology("test", conf, builder.createTopology());
+
+		Utils.sleep(32000);
+		cluster.killTopology("test");
+		cluster.shutdown();
+		for(Entry<String, Integer> entry: MyBolt.ip.getTopIP().entrySet()) {
+			System.out.println(entry.getKey() + "\t:"+entry.getValue());
+		}
+	}
 }
