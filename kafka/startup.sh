a1f81489ceac9a1d12d3aff3a8b4e29df80d19ee
Fei Lee <lee.lfff@gmail.com>
2014-08-25 16:49:08 +0800

    lee

--- kafka/startup.sh
+++ kafka/startup.sh
@@ -10,20 +10,21 @@ cp /opt/kafka/config/server.properties /opt/kafka/config/server-1.properties
 cp /opt/kafka/config/server.properties /opt/kafka/config/server-2.properties 
 sed -i "s/^broker.id=0/broker.id=1/" /opt/kafka/config/server-1.properties
 sed -i "s/^port=9092/port=9093/" /opt/kafka/config/server-1.properties
-sed -i "s/^log.dirs=/tmp/kafka-logs/log.dirs=/tmp/kafka-logs-1/" /opt/kafka/config/server-1.properties
+sed -i "s/^log.dirs=\/tmp\/kafka-logs/log.dirs=\/tmp\/kafka-logs-1/" /opt/kafka/config/server-1.properties
 sed -i "s/^broker.id=0/broker.id=2/" /opt/kafka/config/server-2.properties
 sed -i "s/^port=9092/port=9094/" /opt/kafka/config/server-2.properties
-sed -i "s/^log.dirs=/tmp/kafka-logs/log.dirs=/tmp/kafka-logs-2/" /opt/kafka/config/server-2.properties
+sed -i "s/^log.dirs=\/tmp\/kafka-logs/log.dirs=\/tmp\/kafka-logs-2/" /opt/kafka/config/server-2.properties
 
 #start kafka server
 service supervisor start
-sleep 1s
+sleep 5s
 
 #create a topic "test" with single partion and only one replica
 bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 2 --partitions 20 --topic $TOPIC
 
 #change to foreground
-service supervisor stop
+#service supervisor stop
+supervisorctl stop all
+supervisorctl shutdown
 sed -i "s/nodaemon=false/nodaemon=true/" /etc/supervisor/conf.d/supervisord.conf
-sleep 1s
 service supervisor start
